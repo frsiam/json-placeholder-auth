3e045793d2ffdc91284df667fb8103190273887d
frsiam <siam.frs@gmail.com>
2022-04-16 17:44:27 +0600

    show the user data into home

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,16 +1,20 @@
+import { Link } from 'react-router-dom';
 import useUsers from '../../Hooks/useUsers';
 import SingleUser from '../../SingleUser/SingleUser';
 
 const Home = () => {
     const userData = useUsers('https://jsonplaceholder.typicode.com/users')
-    if(userData.length){
+    if (userData.length) {
         userData.length = 4;
     }
     return (
-        <div>
-            {
-                userData.map(user => <SingleUser user={user}/>)
-            }
+        <div className='container mx-auto'>
+            <div className='row'>
+                {
+                    userData?.map(user => <SingleUser key={user.id} user={user} />)
+                }
+            </div>
+            <Link className='h4' to='/users'>Loading.....</Link>
         </div>
     );
 };
--- src/pages/SingleUser/SingleUser.js
+++ src/pages/SingleUser/SingleUser.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const SingleUser = ({users}) => {
-    
+const SingleUser = ({ user }) => {
+    const {name,id} = user || {}
     return (
-        <div>
-            <h1>User{users}</h1>
+        <div className='col-4 g-4'>
+            <div className="card shadow-sm p-3 mb-5 bg-body rounded">
+                <div className="card-body">
+                    <h5 className="card-title">Name: {name}</h5>
+                    <h6 className="card-subtitle mb-2 text-muted">Id: {id}</h6>
+                    <Link to="/" className="card-link">Card link</Link>
+                </div>
+            </div>
         </div>
     );
 };
